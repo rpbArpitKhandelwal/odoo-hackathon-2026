9115fe264e704cbc7fb0c85b7c1a6e72233860a6
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-07-12 15:53:10 +0530

    chore: clean up all project files, keeping only the folder structure

--- backend/src/lib/prisma.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-// Single shared client (avoids exhausting Postgres connections during dev hot-reloads)
-const prisma = new PrismaClient();
-
-module.exports = prisma;
--- backend/src/server.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`TransitOps API running on http://localhost:${PORT}`));
